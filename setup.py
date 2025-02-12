1e9f8d0fa140f6333f8887d179d0b5da5df6efe3
aniketjayant953 <aniketjayant953@gmail.com>
2025-02-12 10:37:32 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A mini-project combining all we have learnt in ML part in ML-OPs',
+    author='Aniket',
+    license='',
+)
