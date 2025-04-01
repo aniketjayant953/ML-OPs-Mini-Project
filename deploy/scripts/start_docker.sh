3da57578247c8565b00c1005891751b546c5360f
aniketjayant953 <aniketjayant953@gmail.com>
2025-04-01 15:21:03 +0530

    add white background

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -4,7 +4,7 @@
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 864899871537.dkr.ecr.eu-north-1.amazonaws.com
 
 # Pull the latest image
-docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v3
+docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:latest
 
 # Stop the container if it is running
 if [ "$(docker ps -q -f name=campusx-app)" ]; then
@@ -23,4 +23,4 @@ else
 fi
 
 # Run the new container
-docker run -d -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v3
+docker run -d -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:latest
