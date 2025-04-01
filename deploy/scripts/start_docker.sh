36cfd7be83a0a203f4dbfb56ec0b6f550d994ebb
aniketjayant953 <aniketjayant953@gmail.com>
2025-04-01 13:15:44 +0530

    add CodeDeploy scripts

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -4,4 +4,4 @@ aws ecr get-login-password --region eu-north-1 | docker login --username AWS --p
 docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v2
 sudo docker stop campusx-app || true
 sudo docker rm campusx-app || true
-docker run -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v2
+docker run -d -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v2
