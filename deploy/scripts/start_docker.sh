9790b2586890b95810831d53e000814dd4fc1d58
aniketjayant953 <aniketjayant953@gmail.com>
2025-04-01 13:51:16 +0530

    add purple background

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
 # Login to AWS ECR
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 864899871537.dkr.ecr.eu-north-1.amazonaws.com
-docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v2
-sudo docker stop campusx-app || true
-sudo docker rm campusx-app || true
-docker run -d -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v2
+
+# Pull the latest image
+docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v3
+
+# Check if the container  'campusx-app' is running
+if ["$(docker ps -q -f name=campusx-app)"]; then
+    # Stop the container
+    docker stop campusx-app 
+
+# Check if the container 'campusx-app' exists (stopped or running)
+fi ["$(docker ps -q -f name=campusx-app)"]; then
+    # Remove the container if it exists
+    docker rm campusx-app 
+
+
+# Run the new container
+docker run -d -p 80:5000 -e DAGSHUB_PAT=157747ed9e1f44d43b983f67bfb36c12e07602e5 --name campusx-app 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v3
