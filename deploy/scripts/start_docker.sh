1a34d671a529d0b1a4daa9fe25fd041790c08139
aniketjayant953 <aniketjayant953@gmail.com>
2025-04-01 14:15:01 +0530

    add purple background

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -7,13 +7,13 @@ aws ecr get-login-password --region eu-north-1 | docker login --username AWS --p
 docker pull 864899871537.dkr.ecr.eu-north-1.amazonaws.com/emotion-project-ecr:v3
 
 # Check if the container  'campusx-app' is running
-if ["$(docker ps -q -f name=campusx-app)"]; then
+if [ "$(docker ps -q -f name=campusx-app)" ]; then
     # Stop the container
     docker stop campusx-app 
 fi
 
 # Check if the container 'campusx-app' exists (stopped or running)
-if ["$(docker ps -q -f name=campusx-app)"]; then
+if [ "$(docker ps -q -f name=campusx-app)" ]; then
     # Remove the container if it exists
     docker rm campusx-app 
 fi
