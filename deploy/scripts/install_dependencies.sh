aae062e5ffb38139ca421acc51ac9899eedee6a3
aniketjayant953 <aniketjayant953@gmail.com>
2025-04-01 12:16:52 +0530

    add CodeDeploy scripts

--- deploy/scripts/install_dependencies.sh
+++ deploy/scripts/install_dependencies.sh
@@ -1,22 +1,19 @@
-# To run on EC2 as soon its initalized
-
 #!/bin/bash
 
-# Ensure the script runs in non-interactive mode
+# Ensure that the script runs in non-interactive mode
 export DEBIAN_FRONTEND=noninteractive
 
-# Update the package list 
+# Update the package lists
 sudo apt-get update -y
 
 # Install Docker
 sudo apt-get install -y docker.io
 
-# Start and enable docker
-sudo systemctl start docker 
+# Start and enable Docker service
+sudo systemctl start docker
 sudo systemctl enable docker
 
-
-# Install necessaries utilities
+# Install necessary utilities
 sudo apt-get install -y unzip curl
 
 # Download and install AWS CLI
@@ -24,7 +21,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu
 unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu/
 sudo /home/ubuntu/aws/install
 
-# To no use "sudo" again and again
+# Add 'ubuntu' user to the 'docker' group to run Docker commands without 'sudo'
 sudo usermod -aG docker ubuntu
 
 # Clean up the AWS CLI installation files
